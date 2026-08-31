accfd0fe0b628c965962aa235561ad17ac4c20ba
Ara Darbinyan <aradarbinyan.2003@gmail.com>
2026-09-01 00:39:14 +0400

    feat: add supported topic groups

---
+++ bot/constants/topics.py
@@ -0,0 +1,24 @@
+LANGUAGES = {
+    "python": "Python",
+    "java": "Java",
+    "javascript": "JavaScript",
+    "sql": "SQL",
+    "csharp": "C#",
+    "cpp": "C++",
+    "c": "C",
+    "php": "PHP",
+    "go": "Go",
+    "rust": "Rust",
+    "swift": "Swift",
+    "kotlin": "Kotlin",
+}
+
+
+DIRECTIONS = {
+    "ml": "ML",
+    "qa": "QA",
+    "devops": "DevOps",
+    "security": "Info Security",
+    "web": "Web Development",
+    "gamedev": "Game Dev",
+}
