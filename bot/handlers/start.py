1362f58cb1d79d7f13d206332c7bee4f23eedfe9
Ara Darbinyan <aradarbinyan.2003@gmail.com>
2026-08-31 22:50:35 +0400

    refactor: move start handler to separate module, replaced bot.py on main.py

---
+++ bot/handlers/start.py
@@ -0,0 +1,13 @@
+# bot/handlers/start.py
+
+from telegram import Update
+from telegram.ext import ContextTypes
+
+
+async def start(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+    await update.message.reply_text(
+        "Привет!"
+    )
---
+++ main.py
@@ -0,0 +1,26 @@
+import logging
+from telegram.ext import Application, CommandHandler
+
+from config import BOT_TOKEN
+from bot.handlers.start import start
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    application.add_handler(
+        CommandHandler("start", start)
+    )
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
