25faa387cc60e923ac83694560cc84b30ed0f825
Ara Darbinyan <aradarbinyan.2003@gmail.com>
2026-09-04 20:10:57 +0400

    fix: support Railway PostgreSQL URL

--- database/config.py
+++ database/config.py
@@ -12,3 +12,17 @@ if not DATABASE_URL:
     raise RuntimeError(
         "Required environment variable 'DATABASE_URL' is not set"
     )
+
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+asyncpg://",
+        1,
+    )
+
+elif DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql+asyncpg://",
+        1,
+    )
